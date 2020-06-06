a3dcede5f2d294636a276af58791de7a608c32b8
mrkwapo <mrkwapo@gmail.com>
2020-06-06 18:49:08 -0400

    integrated with API GATEWAY

---
+++ src/main/java/com/dracodess/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,17 @@
+package com.dracodess.springsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
+public class SpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+}
